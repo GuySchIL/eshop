204d314e8708fcd1fd425c0bf679d878befdbc87
GuySchIL <guy.sch7@gmail.com>
2023-03-27 19:40:32 +0300

    Basic frontend and backend

--- backend/data.js
+++ backend/data.js
@@ -1,112 +1,111 @@
 const data = {
-    products: [
-      // ProductA
-      {
-        name: 'productA-name',
-        category: 'Pharma',
-        description: '',
-        price: 100,
-        token: 'productA-token',
-        brand: 'Teva',
-        countInStock: 6,
-        rating: 2.5,
-        numReviews: 100,
-        image: '/images/p1.jpg',
-      },
-      //product B
-      {
-        name: 'productB-name',
-        category: 'Pharma',
-        description: '',
-        price: 70,
-        token: 'productB-token',
-        brand: 'Teva',
-        countInStock: 3,
-        rating: 3,
-        numReviews: 95,
-        image: '/images/p2.jpg',
-      },
-      //product C
-      {
-        name: 'productC-name',
-        category: 'Pharma',
-        description: '',
-        price: 10,
-        token: 'productC-token',
-        brand: 'Teva',
-        countInStock: 24,
-        rating: 4,
-        numReviews: 120,
-        image: '/images/p3.jpg',
-      },
-      //product D
-      {
-        name: 'productD-name',
-        category: 'Pharma',
-        description: '',
-        price: 110,
-        token: 'productD-token',
-        brand: 'Teva',
-        countInStock: 7,
-        rating: 4.5,
-        numReviews: 80,
-        image: '/images/p4.jpg',
-      },
-  
-      // ProductA
-      {
-        name: 'productA-name',
-        category: 'Pharma',
-        description: '',
-        price: 100,
-        token: 'productA-token',
-        brand: 'Teva',
-        countInStock: 6,
-        rating: 2.5,
-        numReviews: 100,
-        image: '/images/p1.jpg',
-      },
-      //product B
-      {
-        name: 'productB-name',
-        category: 'Pharma',
-        description: '',
-        price: 70,
-        token: 'productB-token',
-        brand: 'Teva',
-        countInStock: 3,
-        rating: 3,
-        numReviews: 95,
-        image: '/images/p2.jpg',
-      },
-      //product C
-      {
-        name: 'productC-name',
-        category: 'Pharma',
-        description: '',
-        price: 10,
-        token: 'productC-token',
-        brand: 'Teva',
-        countInStock: 24,
-        rating: 4,
-        numReviews: 120,
-        image: '/images/p3.jpg',
-      },
-      //product D
-      {
-        name: 'productD-name',
-        category: 'Pharma',
-        description: '',
-        price: 110,
-        token: 'productD-token',
-        brand: 'Teva',
-        countInStock: 7,
-        rating: 4.5,
-        numReviews: 80,
-        image: '/images/p4.jpg',
-      },
-    ],
-  };
-  
-  export default data;
-  
+  products: [
+    // Product A
+    {
+      name: 'productA-name',
+      category: 'Pharma',
+      description: '',
+      price: 100,
+      token: 'productA-token',
+      brand: 'Teva',
+      countInStock: 6,
+      rating: 2.5,
+      numReviews: 100,
+      image: '/images/p1.jpg',
+    },
+    //product B
+    {
+      name: 'productB-name',
+      category: 'Pharma',
+      description: '',
+      price: 70,
+      token: 'productB-token',
+      brand: 'Teva',
+      countInStock: 3,
+      rating: 3,
+      numReviews: 95,
+      image: '/images/p2.jpg',
+    },
+    //product C
+    {
+      name: 'productC-name',
+      category: 'Pharma',
+      description: '',
+      price: 10,
+      token: 'productC-token',
+      brand: 'Teva',
+      countInStock: 24,
+      rating: 4,
+      numReviews: 120,
+      image: '/images/p3.jpg',
+    },
+    //product D
+    {
+      name: 'productD-name',
+      category: 'Pharma',
+      description: '',
+      price: 110,
+      token: 'productD-token',
+      brand: 'Teva',
+      countInStock: 7,
+      rating: 4.5,
+      numReviews: 80,
+      image: '/images/p4.jpg',
+    },
+
+    // ProductE
+    {
+      name: 'productE-name',
+      category: 'Pharma',
+      description: '',
+      price: 100,
+      token: 'productE-token',
+      brand: 'Teva',
+      countInStock: 6,
+      rating: 2.5,
+      numReviews: 100,
+      image: '/images/p1.jpg',
+    },
+    //product F
+    {
+      name: 'productF-name',
+      category: 'Pharma',
+      description: '',
+      price: 70,
+      token: 'productF-token',
+      brand: 'Teva',
+      countInStock: 3,
+      rating: 3,
+      numReviews: 95,
+      image: '/images/p2.jpg',
+    },
+    //product G
+    {
+      name: 'productG-name',
+      category: 'Pharma',
+      description: '',
+      price: 10,
+      token: 'productG-token',
+      brand: 'Teva',
+      countInStock: 24,
+      rating: 4,
+      numReviews: 120,
+      image: '/images/p3.jpg',
+    },
+    //product H
+    {
+      name: 'productH-name',
+      category: 'Pharma',
+      description: '',
+      price: 110,
+      token: 'productH-token',
+      brand: 'Teva',
+      countInStock: 7,
+      rating: 4.5,
+      numReviews: 80,
+      image: '/images/p4.jpg',
+    },
+  ],
+};
+
+export default data;
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,27 +1,72 @@
+import { useEffect, useReducer, useState } from 'react';
 import { Link } from 'react-router-dom';
 import data from '../data';
+import axios from 'axios';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'GET_REQUEST':
+      return { ...state, loading: true };
+    case 'GET_SUCCESS':
+      return { ...state, products: action.payload, loading: false };
+    case 'GET_FAILURE':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
 function HomePage() {
+  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
+    loading: true,
+    error: '',
+    products: [],
+  });
+
+  // const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      dispatch({ type: 'GET_REQUEST' });
+      try {
+        const res = await axios.get('/api/v1/products');
+        dispatch({ type: 'GET_SUCCESS', payload: res.data });
+      } catch (error) {
+        dispatch({ type: 'GET_FAILURE', payload: error.message });
+      }
+
+      // setProducts(res.data);
+    };
+
+    getProducts();
+  }, []);
+
   return (
     <div>
       <h1>Products</h1>
       <div className="products">
-        {data.products.map((product) => (
-          <div key={product.token} className="product">
-            <Link to={`/product/${product.token}`}>
-              <img src={product.image} alt={product.name}></img>
-            </Link>
-            <div className="product-desc">
+        {loading ? (
+          <p>Loading...</p>
+        ) : error ? (
+          <p>{error}</p>
+        ) : (
+          products.map((product) => (
+            <div key={product.token} className="product">
               <Link to={`/product/${product.token}`}>
-                <p>{product.name}</p>
+                <img src={product.image} alt={product.name}></img>
               </Link>
-              <p>
-                <strong>{product.price}$</strong>
-              </p>
-              <button>Add To Cart</button>
+              <div className="product-desc">
+                <Link to={`/product/${product.token}`}>
+                  <p>{product.name}</p>
+                </Link>
+                <p>
+                  <strong>{product.price}$</strong>
+                </p>
+                <button>Add To Cart</button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
